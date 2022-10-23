2afe6b8246756194385208363959bada0eeb2235
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-10-23 20:31:29 +0700

    update db pantient

--- src/main/java/se331/rest/config/InitApp.java
+++ src/main/java/se331/rest/config/InitApp.java
@@ -68,6 +68,182 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                 tempPeople.setVac_date("January 22, 2021");
                 tempPeople.getVaccines().add(vac2);
                 tempPeople.setVac_date("March 22, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Bodhi")
+                .surname("Buxton")
+                .age(19L)
+                .hometown("87 Tailwater Ave. Lawrence, MA 01841")
+                .vaccinated(true)
+                .build());
+
+                tempPeople.getVaccines().add(vac1);
+                tempPeople.setVac_date("February 9, 2021");
+                tempPeople.getVaccines().add(vac2);
+                tempPeople.setVac_date("June 11, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Hakim")
+                .surname("Schwartz")
+                .age(18L)
+                .hometown("56 Temple Dr. Melrose, MA 02176")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("February 26, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("June 16, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Ezekiel")
+                .surname("Sanchez")
+                .age(25L)
+                .hometown("316 Belmont Lane Longwood, FL 32779")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("July 1, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("October 5, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Dayna")
+                .surname("John")
+                .age(22L)
+                .hometown("593 Purple Finch Drive Brooklyn, NY 11201")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("October 30, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("January 16, 2022");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Devonte")
+                .surname("Corona")
+                .age(30L)
+                .hometown("7618 Ridge St. Abingdon, MD 21009")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("January 6, 2022");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("May 1, 2022");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Tatiana")
+                .surname("Aksel")
+                .age(28L)
+                .hometown("723 North Carson Dr. Owensboro, KY 42301")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("January 16, 2022");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Evelyn")
+                .surname("Horne")
+                .age(32L)
+                .hometown("8657 Prairie Drive Reynoldsburg, OH 43068")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("December 22, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Anisah")
+                .surname("Wagner")
+                .age(36L)
+                .hometown("330 N. Monroe Lane Clarksburg, WV 26301")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("June 28, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("October 28, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Thompson")
+                .surname("Merritt")
+                .age(42L)
+                .hometown("162 Chestnut Ave. Marquette, MI 49855")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("August 27, 2020");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("December 28, 2020");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Julian")
+                .surname("Rollins")
+                .age(39L)
+                .hometown("8652 Hillcrest Dr. Brownsburg, IN 46112")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("February 26, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("April 26, 2021");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Caitlin")
+                .surname("Schneider")
+                .age(30L)
+                .hometown("9259 West James Street Cantonment, FL 32533")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("March 22, 2020");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Eshal")
+                .surname("Villa")
+                .age(38L)
+                .hometown("499 Henry Road Commack, NY 11725")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("June 29, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("May 12, 2022");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("John-Paul")
+                .surname("Villa")
+                .age(36L)
+                .hometown("499 Henry Road Commack, NY 11725")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("January 2, 2020");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("August 22, 2020");
+
+        tempPeople = peopleRepository.save(People.builder()
+                .name("Wong")
+                .surname("Asiyah")
+                .age(31L)
+                .hometown("17 Birchpond St. Bethesda, MD 20814")
+                .vaccinated(true)
+                .build());
+
+        tempPeople.getVaccines().add(vac1);
+        tempPeople.setVac_date("April 10, 2021");
+        tempPeople.getVaccines().add(vac2);
+        tempPeople.setVac_date("December 30, 2021");
     }
         // Authority configuration
         // Doctor is an admin, Patient is a user, Neither are disableUser
